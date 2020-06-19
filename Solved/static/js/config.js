e3ff1a35ca72766f96995fdc91e526b897a247e0
Daniel Tsai <59071439+daniel8691@users.noreply.github.com>
2020-06-19 14:54:28 -0400

    COVID_death_rate_visualized

---
+++ Solved/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZHRzYWkiLCJhIjoiY2theDNoZHY4MDJ4aDJ3b2psYWxyZGg5aCJ9.5kOZpBNuyubEkQ0-6Upyvg"
---
+++ covid_data.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Instructions/Resources/hawaii.sqlite")
+
+#reflect an existing database into a new model
+Base = automap_base()
+
+#reflect the tables
+
+Base.prepare(engine, reflect=True)
